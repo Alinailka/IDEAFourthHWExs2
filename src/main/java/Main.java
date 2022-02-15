83670aa0cb862e966c75f216ee9e1b71d23d7d5b
Alinailka <alina-il@mail.ru>
2022-02-16 00:04:57 +0700

    fixes #1 Order form added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
-
-    long amount = 1000_60;
-    boolean registered = true;
-
-    service.calculate(amount, registered);
-  }
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long bonus = service.calculate(100_00_65, true);
+        System.out.println("Ваш бонус составил " + bonus);
+    }
 }
+
 
